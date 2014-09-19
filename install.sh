b71cb16e40c80483154bab27da3074089056318d
Grieverheart <nicktasios@gmail.com>
2014-09-19 23:19:20 +0200

    Fixed submodule path and install script.

--- install.sh
+++ install.sh
@@ -2,11 +2,10 @@
 
 echo === Checking out vundle
 git submodule update --init
-cd -
 
 echo === Linking vim directories
 ln -s $PWD/.vimrc $HOME/.vimrc
-ln -s $PWD $HOME/.vim
+ln -s $PWD/.vim $HOME/.vim
 
 echo === Linking tmux configuration
 ln -s $PWD/.tmux.conf $HOME/.tmux.conf
